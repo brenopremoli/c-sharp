e4804470528c3c87bb77979b998cae92630dbb52
Breno <breno.oliveira463@gmail.com>
2022-08-24 17:50:19 -0300

    C#

--- c#/exercicios/parte1/002/Program.cs
+++ c#/exercicios/parte1/002/Program.cs
@@ -1,4 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+/* 2- Escreva um programa semelhante ao do exercício 1, mas agora o número de notas e pesos
+pode variar. O usuário deve digitar quantas notas ele desejar e, para parar, a nota -1 deve ser
+digitada. Neste momento a média das notas e pesos digitados anteriormente deve ser
+calculada e o resultado impresso na tela.  */
 double somaCalculo = 0.0, somaPeso = 0.0;
 int i = 1;
 
---
+++ c#/exercicios/parte1/004/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+int n = 10;
+long fat = 1;
+int cont = 1;
+while (cont <= n)
+{
+    fat *= cont;
+    cont++;
+}
+Console.WriteLine("Fatorial de " + n + " = " + fat);
+
+// Outro jeito
+Console.WriteLine();
+int valor = 10;
+long resposta = 1;
+for (int i = 2; i <= valor; i++) {
+	resposta *= i;
+}
+Console.WriteLine("O fatorial de " + valor + " igual a " + resposta);
+
