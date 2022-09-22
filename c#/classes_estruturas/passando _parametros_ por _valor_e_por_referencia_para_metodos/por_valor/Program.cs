020206b8427d62d257d908d0cb21bc156d1a365b
Breno <breno.oliveira463@gmail.com>
2022-09-22 16:40:47 -0300

    C#

---
+++ c#/classes_estruturas/passando _parametros_ por _valor_e_por_referencia_para_metodos/por_valor/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
+
+MyClass m = new MyClass();
+
+int i = 5;
+m.Trocar(i);
+
+Console.WriteLine(i);
+
+
+class MyClass
+{
+    public void Trocar(int x)
+    {           // Troca do valor da cópia
+        x = 10; // Mudança só é feita no método
+    } 
+}
+
