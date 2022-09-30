861b057221469652d8aacbfcf40ada7564349249
Breno <breno.oliveira463@gmail.com>
2022-09-30 20:45:48 -0300

    C#

---
+++ c#/2_classes_estruturas/2_5_nullabe_types_e_operador/Program.cs
@@ -0,0 +1,15 @@
+﻿int? n = null; // ? - null
+
+if (n == null)
+{
+    Console.WriteLine("Valor NULO - 'null'");
+}
+
+if (!n.HasValue)
+{
+    Console.WriteLine("Valor NULO - 'null'");
+}
+
+int  x = n ?? 0;  // Se n for "null" x = 0 
+
+Console.WriteLine(x);
---
+++ c#/2_classes_estruturas/2_6_criando_estruturas/Program.cs
@@ -0,0 +1,15 @@
+﻿Ponto p;
+p.x = 10;
+p.y = 20;
+
+p.MostrarCoordenadas();
+struct Ponto
+{
+    public int x;
+    public int y;
+
+    public void MostrarCoordenadas()
+    {
+        Console.WriteLine("x = "+ x + ", y = " + y);
+    }
+}
