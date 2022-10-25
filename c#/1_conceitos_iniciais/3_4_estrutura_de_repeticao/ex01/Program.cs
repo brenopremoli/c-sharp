2f4f8d2c518ed804da721dda5eccf3daa76061bf
Breno <breno.oliveira463@gmail.com>
2022-10-25 17:12:06 -0300

    C#

--- c#/1_conceitos_iniciais/3_4_estrutura_de_repeticao/ex01/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-int x = 0;
-
-while (x <= 10)
-{
-    Console.WriteLine(x);
-    x++; // x = x + 1 // x += 1
-}
---
+++ c#/aula02/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("CFB Cursos");
+if(args.GetLength(0)>0){
+    Console.Write(args.GetValue(0));
+}
