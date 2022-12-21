7690cd57d2f2d1537c3b18aecc75bb507875b94d
Breno <breno.oliveira463@gmail.com>
2022-12-21 18:24:39 -0300

    C#

--- c#/aula19/Program.cs
+++ c#/aula19/Program.cs
@@ -1,1 +1,13 @@
 ﻿// Loop FOR / Estruturas de iteração
+
+int[] num = new int[5];
+
+for(int i = 0; i < num.Length; i++) // inicializador/contador - qtd. de repetição/condição -  incremento/decremento
+{
+    num[i] = i;
+}
+
+for(int i = 0; i < num.Length; i++) 
+{
+    Console.WriteLine(num[i]);
+}
---
+++ c#/aula20/Program.cs
@@ -0,0 +1,13 @@
+﻿// Loop WHILE / Estruturas de iteração
+
+int[] num = new int[5];
+
+int i = num.Length - 1;
+
+while(i > 0) // condição
+{
+    num[i] = 0;
+    Console.WriteLine("Olá Mundo!");
+    i--;
+}
+Console.WriteLine("FIM");
