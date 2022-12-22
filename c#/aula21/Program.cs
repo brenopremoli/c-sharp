730f6023ae843e9831be2c122730e271abe80fa9
Breno <breno.oliveira463@gmail.com>
2022-12-22 17:20:49 -0300

    C#

---
+++ c#/aula21/Program.cs
@@ -0,0 +1,25 @@
+﻿// Loop DO WHILE / Estruturas de iteração 
+
+int num = 5;
+
+do 
+{
+    Console.WriteLine("Olá Mundo!");
+}
+while(num < 5);
+
+string senha = "123";
+string digitada;
+int tentativa = 0;
+
+
+do 
+{
+    Console.Clear();
+    Console.Write("Digite a senha: ");
+    digitada = Console.ReadLine();
+    tentativa++;
+}
+while(senha != digitada);
+
+Console.WriteLine("Senha Correta! Tentativas: {0} ",tentativa);
---
+++ c#/aula22/Program.cs
@@ -0,0 +1,14 @@
+﻿// Loop FOREACH / Estruturas de iteração
+
+int[] num = new int[5]{11, 22, 33, 44, 55};
+
+for (int i = 0; i < num.Length; i++) // inicializa/atribui - controle de posições
+{
+    num[i] = 0;
+}
+
+
+foreach(int n in num) // leitura
+{
+    Console.WriteLine(n);
+}
