f8b53a08e7f4b86239191e29ff2d1828f2e7f087
Breno <breno.oliveira463@gmail.com>
2022-12-26 11:24:11 -0300

    C#

---
+++ c#/aula24/Program.cs
@@ -0,0 +1,36 @@
+﻿// Métodos
+
+static void ola() // void = sem retorno
+{
+    Console.WriteLine("Olá Mundo!");
+    Console.WriteLine("Curso de C#");
+}
+
+static void soma(int x, int y)
+{
+    Console.WriteLine("Resultado da soma de {0} + {1} = {2}", x, y, x+y);
+}
+
+static int somaRetorno(int x, int y) // int = com retorno
+{
+    int res = x + y;
+    return res;
+}
+
+static double nome(int n1, double n2, string texto)
+{
+    double t = 0;
+    return t;
+}
+
+/*
+int n1, n2;
+n1 = Convert.ToInt32(Console.ReadLine());
+n2 = Convert.ToInt32(Console.ReadLine());
+soma(n1, n2);
+*/
+
+int n1, n2;
+n1 = Convert.ToInt32(Console.ReadLine());
+n2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Resultado da soma de {0} + {1} = {2}", n1, n2, somaRetorno(n1, n2));
