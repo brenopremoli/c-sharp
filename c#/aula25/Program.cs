40b62daadb41bc2b4c453aea2527ae0f503f1889
Breno <breno.oliveira463@gmail.com>
2022-12-28 16:47:55 -0300

    C#

---
+++ c#/aula25/Program.cs
@@ -0,0 +1,18 @@
+﻿// Passagem por valor e por referência
+
+static void dobrar1(ref int valor)
+{
+    valor *= 2;
+}
+
+static void dobrar2(int valor)
+{
+    valor *= 2;
+}
+
+int n1 = 50;
+dobrar1(ref n1);
+Console.WriteLine(n1);
+int n2 = 50;
+dobrar2(n2);
+Console.WriteLine(n2);
---
+++ c#/aula26/Program.cs
@@ -0,0 +1,15 @@
+﻿// Argumento out
+
+static int divide(int dividendo, int divisor, out int resto)
+{
+    int quociente = dividendo / divisor;
+    resto = dividendo % divisor;
+    return quociente;
+}
+
+int divid, divis, quoc, rest;
+divid = 10;
+divis = 3;
+quoc = divide(divid, divis, out rest);
+
+Console.WriteLine("{0}/{1} \nQuociente = {2} \nResto = {3} ", divid, divis, quoc, rest);
