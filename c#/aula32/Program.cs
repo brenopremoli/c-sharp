8030340c392d1822396a074350f6e0f798696117
Breno <breno.oliveira463@gmail.com>
2023-01-04 17:36:30 -0300

    C#

---
+++ c#/aula32/Program.cs
@@ -0,0 +1,21 @@
+﻿// This 
+
+Calculos c = new Calculos(10,2);
+Console.WriteLine(c.Somar());
+
+class Calculos{
+    public int v1; // "this" pertence
+    public int v2; // "this" pertence
+
+    public Calculos(int v1, int v2)
+    {
+        this.v1 = v1;
+        this.v2 = v2;
+    }
+
+    public int Somar()
+    {
+        return v1 + v2;
+    }
+}
+
---
+++ c#/aula33/Program.cs
@@ -0,0 +1,54 @@
+﻿// Public vs Private
+
+Jogador j1 = new Jogador("Breno");
+
+j1.setEnergia(-40);
+j1.setEnergia(60);
+
+Console.WriteLine("Nome:    {0}", j1.getNome());
+Console.WriteLine("Energia: {0}", j1.getEnergia());
+
+class Jogador{
+    private int energia;
+    private string nome;
+    public Jogador(string nome){
+        this.nome = nome;
+        energia = 100;
+    }
+
+    public int getEnergia() // get -> obter -> método para obter o valor
+    {
+        return energia;
+    }
+
+    public string getNome() // get -> obter -> método para obter o valor
+    {
+        return nome;
+    }
+
+    public void setEnergia(int e) // set -> alterar ->método para alterar o valor
+    {
+        if (e < 0)
+        {
+            if (energia + e < 0)
+            {
+                energia = 0;
+            }
+            else
+            {
+                energia = energia + e;
+            }
+        }
+        else if (e > 0)
+        {
+            if (energia + e > 100)
+            {
+                energia = 100;
+            }
+            else
+            {
+                energia = energia + e;
+            }
+        }
+    }
+}
