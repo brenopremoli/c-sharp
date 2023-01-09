bffb5794d48d099dc25b07ee0652da81f943fd66
Breno <breno.oliveira463@gmail.com>
2023-01-09 17:16:56 -0300

    C#

---
+++ c#/aula36/Program.cs
@@ -0,0 +1,38 @@
+﻿// Membros Protected -> semelhante ao private
+
+Carro carro = new Carro("Bonitão", 120);
+
+Console.WriteLine("Nome:              {0}", carro.nome);
+Console.WriteLine("Velocidade Máxima: {0}", carro.getVelMax());
+Console.WriteLine("Ligado:           {0}", carro.getLigado());
+
+class Veiculo // classe Base
+{
+    public int velAtual;
+    private int velMax; // restringe o acesso somente a própria classe
+    protected bool ligado; // permite acesso aos membros pelas classes derivadas e pela própria classe
+    public Veiculo(int velMax)
+        {
+            velAtual = 0;
+            this.velMax = velMax;
+            ligado = false; 
+        }
+        public bool getLigado()
+        {
+            return ligado;
+        }
+        public int getVelMax()
+        {
+            return velMax;
+        }
+}
+
+class Carro: Veiculo // Derivada de veiculo
+{
+    public string nome;
+    public Carro(string nome, int vm): base(vm) // vm = velMax
+    {
+        this.nome = nome;
+        ligado = true; // protected 
+    }
+}
---
+++ c#/aula37/Program.cs
@@ -0,0 +1,29 @@
+﻿// Herança/Ordem de execução dos construtores
+
+// Ordem -> Base para a derivada
+
+Derivada2 derivada2 = new Derivada2();
+
+class Base 
+{
+    public Base()
+    {
+        Console.WriteLine("Construtor da classe Base");
+    }
+}
+class Derivada1: Base 
+{
+    public Derivada1()
+    {
+        Console.WriteLine("Construtor da classe Derivada1");
+    }
+}
+class Derivada2: Derivada1 
+{
+    public Derivada2()
+    {
+        Console.WriteLine("Construtor da classe Derivada2");
+    }
+}
+
+
