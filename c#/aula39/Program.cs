1f421b3699018db7e9cba6df4890c8ae9ad9fb01
Breno <breno.oliveira463@gmail.com>
2023-01-11 16:58:11 -0300

    C#

---
+++ c#/aula39/Program.cs
@@ -0,0 +1,45 @@
+﻿// Classes e métodos abstratos
+
+// Base/referência para outras classes que vão herdar a classe abstrata, servem para ser herdadas
+
+Carro carro1 = new Carro();
+
+carro1.aceleracao(1);
+carro1.aceleracao(-1);
+carro1.aceleracao(1);
+
+Console.WriteLine(carro1.getVelAtual());
+
+abstract class Veiculo // Classe base abstrata
+{
+    protected int velMax;
+    protected int velAtual;
+    protected bool ligado;
+    public Veiculo()
+    {
+        ligado = false;
+        velAtual = 0;
+    }
+    public void setLigado(bool ligado)
+    {
+        this.ligado = ligado;
+    }
+    public int getVelAtual()
+    {
+        return velAtual;
+    }
+
+    abstract public void aceleracao(int multi);
+}
+
+class Carro: Veiculo
+{
+    public Carro()
+    {
+        velMax = 120;
+    }
+    override public void aceleracao(int multi)
+    {
+        velAtual += 10 *multi;
+    }
+}
---
+++ c#/aula40/Program.cs
@@ -0,0 +1,10 @@
+﻿// Classe Sealed 
+
+// Não permite que a classe seja herdada por uma classe derivada
+
+Carro c1 = new Carro();
+
+sealed class Veiculo
+{
+
+}
