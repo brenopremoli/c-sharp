09bec018ed91c942006047febee05b3ac0a36305
Breno <breno.oliveira463@gmail.com>
2023-01-12 16:53:47 -0300

    C#

---
+++ c#/aula41/Program.cs
@@ -0,0 +1,44 @@
+﻿// Acessors GET e SET
+
+// Como se fosse uma propriedade junto com um método no mesmo elemento, em uma propriedade privada -> obter, alterar (Calcular)
+
+// get - propriedade somente de leitura
+// set - propriedade somente de escrita
+// get e set - propriedade de leitura e escrita
+
+Carro c1 = new Carro();
+
+c1.vm = 400;
+Console.WriteLine("Velocidade: {0}", c1.vm);
+
+class Carro
+{
+    private int velMax;
+
+    public int vm
+    {
+        get // propriedade somente de leitura
+        {
+            return velMax;
+        }
+        set // propriedade somente de escrita
+        {
+            if (value < 0)
+            {
+                velMax = 0;
+            }
+            else if (value > 300)
+            {
+                velMax = 300;
+            }
+            else
+            {
+                velMax = value;
+            }
+        }
+    }
+    public Carro() // Construtor
+    {
+        vm = 120;
+    }
+}
---
+++ c#/aula42/Program.cs
@@ -0,0 +1,39 @@
+﻿// Indexadores de Classes
+
+// Membro de uma classe que permite aos objetos desta classe ser indexados como se fossem arrays
+
+Carro c1 = new Carro();
+
+c1[4] = 200;
+Console.WriteLine("Velocidade: {0}", c1[4]);
+
+class Carro
+{
+    private int[] velMax = new int[5]{80, 120, 160, 240, 300};
+
+    public int this[int i]
+    {
+        get // propriedade somente de leitura
+        {
+            return velMax[i];
+        }
+        set // propriedade somente de escrita
+        {
+            if (value < 0)
+            {
+                velMax[i] = 0;
+            }
+            else if (value > 300)
+            {
+                velMax[i] = 300;
+            }
+            else
+            {
+                velMax[i] = value;
+            }
+        }
+    }
+    public Carro() // Construtor
+    {
+    }
+}
