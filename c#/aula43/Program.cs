72b5db7c72a750fadab5b49e4dc161718949da05
Breno <breno.oliveira463@gmail.com>
2023-01-13 17:50:30 -0300

    C#

---
+++ c#/aula43/Program.cs
@@ -0,0 +1,47 @@
+﻿// Interfaces 
+
+Carro c = new Carro();
+
+public interface Veiculo
+{
+    void ligar();
+    void desligar();
+    void info();
+}
+
+public interface Combate
+{
+    void disparar();
+}
+
+class Carro: Veiculo, Combate
+{
+    public bool ligado;
+    private int municao;
+    public Carro()
+    {
+        setMunicao(100);
+    }
+
+    public void setMunicao(int qtde)
+    {
+        municao = qtde;
+    }
+
+    public void ligar()
+    {
+        this.ligado = true;
+    }
+    public void desligar()
+    {
+        this.ligado = false;
+    }
+    public void info()
+    {
+
+    }
+    public void disparar()
+    {
+        
+    }
+}
