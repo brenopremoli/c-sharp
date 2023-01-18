1d71168b86a705ac183d2fd1514be2239aefe3de
Breno <breno.oliveira463@gmail.com>
2023-01-18 18:18:23 -0300

    C#

---
+++ c#/aula47/Program.cs
@@ -0,0 +1,31 @@
+﻿// Sobrecarga de métodos
+
+// double res;
+
+Calc  c = new Calc();
+
+var res = c.soma(10.2, 5.4, 7.3, 8.3, 54.2); // var = coringa
+
+Console.WriteLine("Soma: {0}", res);
+
+class Calc
+{
+    public int soma(params int[]n)
+    {
+        int s = 0;
+        for(int i = 0; i < n.Length; i++)
+        {
+            s += n[i];
+        }
+        return s;
+    }
+    public double soma(params double[]n)
+    {
+        double s = 0;
+        for(int i = 0; i < n.Length; i++)
+        {
+            s += n[i];
+        }
+        return s;
+    }
+}
