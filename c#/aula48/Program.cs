58e7deb652a71b718ba1acac98bb5ff4e313687a
Breno <breno.oliveira463@gmail.com>
2023-01-19 14:38:12 -0300

    C#

---
+++ c#/aula48/Program.cs
@@ -0,0 +1,46 @@
+﻿// Recursividade 
+
+// A função chama ela mesma
+
+Calc  c = new Calc();
+
+var res = c.fat(9); // var = coringa
+
+Console.WriteLine("Fatorial: {0}", res);
+
+class Calc
+{
+    public int soma(params int[]n)
+    {
+        int s = 0;
+        for(int i = 0; i < n.Length; i++)
+        {
+            s += n[i];
+        }
+        return s;
+    }
+    public double soma(params double[]n)
+    {
+        double s = 0;
+        for(int i = 0; i < n.Length; i++)
+        {
+            s += n[i];
+        }
+        return s;
+    }
+
+    // 4! = 4*3*2*1
+    public int fat(int n)
+    {
+        int res;
+        if(n <= 1)
+        {
+            res = 1;
+        }
+        else
+        {
+            res = n * fat(n - 1);
+        }
+        return res;
+    }
+}
---
+++ c#/aula49/Program.cs
@@ -0,0 +1,17 @@
+﻿// Métodos e Variáveis estáticos (static) -> usar em qualquer lugar do programa
+
+double vpi = Mat.pi; // pi -> vantagem do static
+int num = 10;
+
+// Mat m = new Mat();
+
+Console.WriteLine("Pi: {0}", vpi);
+Console.WriteLine("Dobro: {0}", Mat.dobro(num));
+
+class Mat{
+    public static double pi = 3.14;
+
+    public static int dobro(int n){
+        return n * 2;
+    }
+}
