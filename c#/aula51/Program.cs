800936067d079dc386a68c81a7057c718128e882
Breno <breno.oliveira463@gmail.com>
2023-01-24 15:39:48 -0300

    C#

--- c#/aula51/Program.cs
+++ c#/aula51/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Argumentos de entrada do programa
+
+int res = 0;
+
+if(args.Length > 0){
+    Console.WriteLine("Quantidade de argumentos: {0}", args.Length);
+    for(int i = 0; i < args.Length; i++){
+           res += Int32.Parse(args[i]);
+    }
+    Console.WriteLine("Soma: {0}", res);
+}else{
+    Console.WriteLine("Não foram passados argumentos");
+}
---
+++ c#/aula52/Program.cs
@@ -0,0 +1,20 @@
+﻿// Exceções (erro) - Try Catch Finally -> tratamento de exceções
+
+// Exceção -> derivada da class "exception" 
+
+int n1, n2, res;
+res = n1 = n2 = 0;
+
+n1 = 10;
+n2 = 0;
+
+try{
+    res = n1 / n2;
+    Console.WriteLine("{0}/{1} = {2}", n1, n2, res);
+}catch(Exception e){
+    Console.WriteLine("ERRO            -> {0}", e.Message);
+    Console.WriteLine("Exceção         -> {0}", e);
+    Console.WriteLine("Tipo da Exceção -> {0}", e.GetType());
+}
+
+
