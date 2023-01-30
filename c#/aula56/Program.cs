b1dd31ca73253a796d0509ba5fc1f17bdb68cb73
Breno <breno.oliveira463@gmail.com>
2023-01-30 19:43:25 -0300

    C#

---
+++ c#/aula56/Program.cs
@@ -0,0 +1,35 @@
+﻿// LinkedList / Lista duplamente encadeada
+
+// Cada elemento é um nó da lista que tem link para o elemento anterior e posterior
+
+LinkedList<string> transp = new LinkedList<string>();
+
+transp.AddFirst("Carro");
+transp.AddFirst("Avião");
+transp.AddFirst("Navio");
+transp.AddFirst("Moto"); // Início
+
+transp.AddLast("Bicicleta"); // Fim
+
+LinkedListNode<string> no;
+no = transp.FindLast("Navio").Next; // next -> aponta para o próximo valor
+no = transp.FindLast("Navio").Previous; // value -> aponta o valor anterior
+transp.AddAfter(no, "Patinete"); // Após
+no = transp.FindLast("Navio");
+transp.AddBefore(no, "Patins"); // Antes
+
+// transp.Clear(); // apaga tudo
+
+if (transp.Find("Carro") == null){
+    Console.WriteLine("Não Encontrado");
+}else{
+    Console.WriteLine("Encontrado");
+}
+
+// transp.Remove("Navio"); // apaga o valor escrito
+transp.RemoveFirst(); // remove o primeiro
+transp.RemoveLast(); // remove o último 
+
+foreach(string t in transp){
+    Console.WriteLine("Transporte: {0}", t);
+}
